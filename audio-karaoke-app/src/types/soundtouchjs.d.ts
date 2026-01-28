a6ea449b534c2d8fdf39eda9edfb8f7cd632572b
bise8128-sketch <bise8128@gmail.com>
2026-01-28 03:26:01 +0100

    ff

--- audio-karaoke-app/src/types/soundtouchjs.d.ts
+++ audio-karaoke-app/src/types/soundtouchjs.d.ts
@@ -1,9 +1,11 @@
 
 declare module 'soundtouchjs' {
     export class PitchShifter {
-        constructor(context: AudioContext, buffer: AudioBuffer, bufferSize: number);
+        constructor(sampleRate: number, length: number, channels: number);
         pitch: number;
         tempo: number;
+        process(input: any): any;
+        flush(): any;
         on(event: string, callback: (data: any) => void): void;
         connect(node: AudioNode): void;
         disconnect(): void;
