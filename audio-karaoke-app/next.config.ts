483fb366ba1202adb5be8948104b6fcaec11b822
bise8128-sketch <bise8128@gmail.com>
2026-02-09 02:10:52 +0100

    gdfgfd

--- audio-karaoke-app/next.config.ts
+++ audio-karaoke-app/next.config.ts
@@ -8,7 +8,7 @@ import CopyWebpackPlugin from 'copy-webpack-plugin';
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname, '../../'),
+  // outputFileTracingRoot: path.join(__dirname, '../../'),
   compress: true,
   // Add allowedDevOrigins to enable cross-origin requests from specific development origins.
   allowedDevOrigins: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://192.168.2.190:3000'],
