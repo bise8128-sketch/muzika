fe43937ac4bcb4f9d6a4498628537daae60fccc7
bise8128-sketch <bise8128@gmail.com>
2026-02-13 22:16:59 +0100

    gggg

--- audio-karaoke-app/playwright.config.ts
+++ audio-karaoke-app/playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     workers: process.env.CI ? 1 : undefined,
     reporter: 'html',
     use: {
-        baseURL: 'http://localhost:3000',
+        baseURL: 'http://localhost:3030',
         trace: 'on-first-retry',
     },
     projects: [
@@ -19,8 +19,8 @@ export default defineConfig({
         },
     ],
     webServer: {
-        command: 'npm run dev',
-        url: 'http://localhost:3000',
+        command: 'npm run dev:next-only',
+        url: 'http://localhost:3030',
         reuseExistingServer: !process.env.CI,
     },
 });
