6a3024512822f5c0d7f71b67de03a6428a83eb4e
bise8128-sketch <bise8128@gmail.com>
2026-02-16 22:52:23 +0100

    jjj

--- audio-karaoke-app/playwright.config.ts
+++ audio-karaoke-app/playwright.config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
+    command: 'npm run dev:next-only',
     url: 'http://localhost:3030',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
