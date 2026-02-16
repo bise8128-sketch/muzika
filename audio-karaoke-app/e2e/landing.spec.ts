07c8f03e8ee3e16d4a44d986489f8179668b179f
bise8128-sketch <bise8128@gmail.com>
2026-02-16 22:54:40 +0100

    dddddd

--- audio-karaoke-app/e2e/landing.spec.ts
+++ audio-karaoke-app/e2e/landing.spec.ts
@@ -4,7 +4,7 @@ test('landing page has title and CTA', async ({ page }) => {
   await page.goto('/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Muzika/i);
+  await expect(page).toHaveTitle(/DaorsKaraoke/i);
 
   // Expect main heading to be visible
   await expect(page.getByRole('heading', { level: 1 })).toBeVisible();
